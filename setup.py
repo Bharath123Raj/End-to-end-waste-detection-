50337829f7b770f19c21e4038c2324b047841b4d
Bharath <jsbharath444@gmail.com>
2024-11-13 12:20:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Bharath Raj',
+    author_email= 'jsbharath444@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
